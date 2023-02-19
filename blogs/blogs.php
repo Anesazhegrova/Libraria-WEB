6f132a7bcc854c1217e93a5332faf04098daf001
BlendaHere <110112052+BlendaHere@users.noreply.github.com>
2023-02-19 20:02:07 +0100

    pjesa e blogut

---
+++ blogs/blogs.php
@@ -0,0 +1,221 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Font awesome icon -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="style.css">
+<?php include_once '../header/header.php'; ?>
+  </head>
+  <body>
+
+  
+
+    
+    <!-- design -->
+    <section class = "design" id = "design">
+      <div class = "container">
+        <div class = "title">
+          <h2><b>Blog</b></h2>
+        </div>
+
+        <div class = "design-content">
+          <!-- item -->
+          <div class = "design-item">
+            <div class = "design-img">
+              <img src = "images/art-design-1.jpg" alt = "">
+              <span><i class = "far fa-heart"></i> 22</span>
+              <span>Read</span>
+            </div>
+            <div class = "design-title">
+              <a href = "#">Sa më shumë të lexoni, aq më shumë gjëra do të dini</a>
+            </div>
+          </div>
+          <!-- end of item -->
+          <!-- item -->
+          <div class = "design-item">
+            <div class = "design-img">
+              <img src = "images/art-design-3.jpeg" alt = "">
+              <span><i class = "far fa-heart"></i> 22</span>
+              <span>Live</span>
+            </div>
+            <div class = "design-title">
+              <a href = "#">Librat janë një magji unike e lëvizshme</a>
+            </div>
+          </div>
+          <!-- end of item -->
+          <!-- item -->
+          <div class = "design-item">
+            <div class = "design-img">
+              <img src = "images/aa.jpg" alt = "">
+              <span><i class = "far fa-heart"></i> 22</span>
+              <span>Read</span>
+            </div>
+            <div class = "design-title">
+              <a href = "#">Një lexues jeton një mijë jetë para se të vdesë, tha Jojen</a>
+            </div>
+          </div>
+          <!-- end of item -->
+          <!-- item -->
+          <div class = "design-item">
+            <div class = "design-img">
+              <img src = "images/mini.jpg" alt = "">
+              <span><i class = "far fa-heart"></i> 22</span>
+              <span>Be Smart</span>
+            </div>
+            <div class = "design-title">
+              <a href = "#">Derisa kisha frikë se do ta humbisja, nuk më pëlqente kurrë të lexoja</a>
+            </div>
+          </div>
+          <!-- end of item -->
+          <!-- item -->
+          <div class = "design-item">
+            <div class = "design-img">
+              <img src = "images/nimi.jpg" alt = "">
+              <span><i class = "far fa-heart"></i> 22</span>
+              <span>Be Sure</span>
+            </div>
+            <div class = "design-title">
+              <a href = "#">Një librari e mirë është thjesht një Vrimë e Zezë e butë që di të lexojë.</a>
+            </div>
+          </div>
+          <!-- end of item -->
+          <!-- item -->
+          <div class = "design-item">
+            <div class = "design-img">
+              <img src = "images/jini.jpg" alt = "">
+              <span><i class = "far fa-heart"></i> 22</span>
+              <span>Explore</span>
+            </div>
+            <div class = "design-title">
+              <a href = "#">Gjithsesi, librat ishin më të sigurt se njerëzit e tjerë.</a>
+            </div>
+          </div>
+          <!-- end of item -->
+        </div>
+      </div>
+    </section>
+    <!-- end of design -->
+
+
+    <!-- about -->
+    <section class = "about" id = "about">
+      <div class = "container">
+        <div class = "about-content">
+          <div>
+            <img src = "images/about-bg.jpg" alt = "">
+          </div>
+          <div class = "about-text">
+            <div class = "title">
+              <h2>Aulona Muller</h2>
+              <p>
+                Libri Është Një Dritë Që Mëson Të Tjerët Për Të Qenë Të Ndritshëm.</p>
+            </div>
+            <p>Vetë kompania është një kompani shumë e suksesshme. Sepse shpeshherë sigurojnë gjithë kënaqësinë e të urtëve, të lindurit dhe madje urren zgjedhja e mundimshme e kohës i pengon ata nga e vërteta, saqë e lënë atë me atë kënaqësi që është i bekuar. Ato sigurojnë nevojat e punës së përmendur!</p>
+            <br>
+            <p>Vetë kompania është një kompani shumë e suksesshme. Të lindur të korruptuar, sepse kanë kënaqësinë të refuzojnë faljen, ofrojnë shërbimin më të vogël, le të jenë një gjë më tej në fluturimin që marrin ato kohë, por duan vërtet, por mbi të gjitha nevojat e atyre që lëvdojini ata!</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- end of about -->
+    
+  </body>
+  <?php
+include_once '../footer/footer.php';
+?>
+</html>
+
+
+<!--<section class="blogs" id="blogs">
+        <h1 class="heading"> <span>Blogu ynë</span> </h1>-->
+    
+        <!--<div class="swiper blogs-slider">-->
+    
+           <!-- <div class="swiper-wrapper">
+    
+                <div class="swiper-slide box">
+                    <div class="image">
+                        <img src="image/blog-1.jpg" alt="">
+                    </div>
+                    <div class="content">
+                        <h3>Aulona Muller</h3>
+                        <p>Libri është një dritë që mëson të tjerët për të qenë të ndritshëm.</p>
+                        <a href="#" class="btn">Lexo më shumë</a>
+                    </div>
+                </div>
+                <br><br> <br><br><br><br><br> <br><br><br><br><br><br> <br><br>
+                
+
+
+
+
+
+    
+                <div class="swiper-slide box">
+                    <div class="image">
+                        <img src="image/blog-2.jpg" alt="">
+                    </div>
+                    <div class="content">
+                        <h3>Milan Alex</h3>
+                        <p>Libri është një dritare në një botë të panjohur.</p>
+                        <br>
+                        <br>
+                        <a href="#" class="btn">Lexo më shumë</a>
+                    </div>
+                </div>
+                <br><br> <br><br><br><br><br> <br><br><br><br><br><br> <br><br>
+                
+
+                <div class="swiper-slide box">
+                    <div class="image">
+                        <img src="image/blog-3.jpg" alt="">
+                    </div>
+                    <div class="content">
+                        <h3>Ana Ribel</h3>
+                        <p>Libri është një mik që na ndihmon të ndjehemi të qetë në një botë të ndërlikuar..</p>
+                        <a href="#" class="btn">Lexo më shumë</a>
+                    </div>
+                </div>
+     <br><br> <br><br><br><br><br> <br><br><br><br><br><br> <br><br>
+                
+                <div class="swiper-slide box">
+                    <div class="image">
+                        <img src="image/blog-4.jpg" alt="">
+                    </div>
+                    <div class="content">
+                        <h3>Diana Braun</h3>
+                        <p>Unë mendoj se libri është një mjet që na ndihmon të kuptojmë më mirë botën në të cilën jetojmë.</p>
+                        <a href="#" class="btn">Lexo më shumë</a>
+                    </div>
+                </div>
+                <br><br> <br><br><br><br><br> <br><br><br><br><br><br> <br><br>
+                
+                <div class="swiper-slide box">
+                    <div class="image">
+                        <img src="image/blog-5.jpg" alt="">
+                    </div>
+                    <div class="content">
+                        <h3>Vesa Lika</h3>
+                        <p>Unë dashuroj të lexoj libra, dhe kjo librari e bën të mundur që të ndaj me të tjerët pasionin tim për lexim.</p>
+                        <a href="#" class="btn">Lexo më shumë</a>
+                    </div>
+                </div>
+    
+           </div>
+    
+       </div>
+       </div>-->
+    
+   <!-- </section>-->
+
+<!-- sektori i  blogjeve mbaron ketu  -->
+
+
+
+
+
